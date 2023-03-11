576ccd45e14f15a314e21cd5010c43b7e9f696fe
Tomáš Jaroš <tjaros.822@gmail.com>
2023-03-11 20:34:45 +0100

    feat: add base class for numeric converters

---
+++ Panbyte/Panbyte/Converters/ByteSequenceConverterBase.cs
@@ -0,0 +1,27 @@
+using System.Numerics;
+using Panbyte.Formats;
+using Panbyte.Formats.Enums;
+using Panbyte.Utils;
+
+namespace Panbyte.Converters;
+
+public abstract class ByteSequenceConverterBase
+{
+    public string BaseConvertTo(byte[] bytes, IFormat outputFormat)
+    {
+        if (bytes.Length == 0 && outputFormat.GetType() != typeof(ByteArray))
+        {
+            return "";
+        }
+        
+        return outputFormat switch
+        {
+            Bytes => new string(bytes.Select(x => (char)x).ToArray()),
+            Hex => Convert.ToHexString(bytes).ToLower(),
+            Bits => string.Join("", bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0'))),
+            Int intFormat => new BigInteger(bytes, true, intFormat.Endianness == Endianness.BigEndian).ToString(),
+            ByteArray arrayFormat => ByteArrayUtils.ConvertToString(bytes, arrayFormat),
+            _ => throw new ArgumentException("Invalid format for conversion")
+        };
+    }
+}
