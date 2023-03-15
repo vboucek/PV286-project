c5b8032c5ea9e670267593300435ae566168016f
Tomáš Jaroš <tjaros.822@gmail.com>
2023-03-15 20:28:29 +0100

    refactor: decompose bits converter

--- Panbyte/Panbyte/Converters/BitsConverter.cs
+++ Panbyte/Panbyte/Converters/BitsConverter.cs
@@ -13,35 +13,41 @@ public class BitsConverter : ByteSequenceConverterBase, IConverter
         InputFormat = format;
     }
 
+    private BigInteger binaryStringToBigInteger(string bits)
+    {
+        BigInteger bigInt = BigInteger.Zero;
+
+        foreach (var bit in bits)
+        {
+            if (bit != '0' && bit != '1')
+            {
+                throw new ArgumentException("Input string contains invalid characters");
+            }
+
+            bigInt <<= 1;
+            if (bit == '1')
+            {
+                bigInt |= BigInteger.One;
+            }
+        }
+
+        return bigInt;
+    }
+
     private byte[] PadAndConvert(string value)
     {
         var stripped = string.Join("", value.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
 
         var padding = ((Bits)InputFormat).BitPadding;
+        var paddingWidth = (stripped.Length % 8 == 0) ? 0 : (8 - stripped.Length % 8);
 
         var bits = padding switch
         {
-            BitPadding.Left => stripped.PadLeft(8 - stripped.Length % 8, '0'),
-            BitPadding.Right => stripped.PadRight(8 - stripped.Length % 8, '0')
+            BitPadding.Left => stripped.PadLeft(stripped.Length + paddingWidth, '0'),
+            BitPadding.Right => stripped.PadRight(stripped.Length + paddingWidth, '0')
         };
         
-        BigInteger bigInt = BigInteger.Zero;
-        foreach (var bit in bits)
-        {
-            switch (bit)
-            {
-                case '0': 
-                    bigInt <<= 1;
-                    break;
-                case '1':
-                    bigInt |= 1;
-                    break;
-                default:
-                    throw new ArgumentException("Invalid format of input value");
-            }
-        }
-
-        return bigInt.ToByteArray(true, true);;
+        return binaryStringToBigInteger(bits).ToByteArray(true, true);
     }
 
     public string ConvertTo(string value, IFormat outputFormat)
