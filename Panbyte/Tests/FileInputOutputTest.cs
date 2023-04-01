e867dd358619b99a115d6af985d0da08f144f47d
Vladimír Bouček <72857024+vladimirboucek@users.noreply.github.com>
2023-04-01 14:18:07 +0200

    feat: added example file test

---
+++ Panbyte/Tests/FileInputOutputTest.cs
@@ -0,0 +1,21 @@
+namespace Tests;
+
+[TestClass]
+public class FileInputOutputTest : RunPanbyteTest
+{
+    [TestMethod]
+    [DeploymentItem(@"TestInputFiles/bytes01.txt")]
+    [DeploymentItem(@"ExpectedOutputFiles/bytes01-to-hex.txt")]
+    public void ConvertBytesInput()
+    {
+        var testInputFile = "TestInputFiles/bytes01.txt";
+        var testOutputFile = "out.txt";
+        var expectedOutputFile = $"ExpectedOutputFiles/bytes01-to-hex.txt";
+        
+        var args = new[] { "-f", "bytes", "-t", "hex", "-i", testInputFile, "-o", testOutputFile };
+        
+        RunPanbyte(args);    
+        
+        Assert.AreEqual(File.ReadAllText(expectedOutputFile), File.ReadAllText(testOutputFile));
+    }
+}
--- Panbyte/Tests/RunPanbyteTest.cs
+++ Panbyte/Tests/RunPanbyteTest.cs
@@ -4,7 +4,7 @@ namespace Tests;
 
 public abstract class RunPanbyteTest
 {
-    protected string RunPanbyte(string consoleInput, string[] args)
+    protected string RunPanbyteWithConsoleInput(string consoleInput, string[] args)
     {
         // Mock console input with StringReader and redirect console output to StringWriter
         using var sw = new StringWriter();
@@ -18,5 +18,11 @@ public abstract class RunPanbyteTest
 
         // Return console result
         return sw.ToString();
+    }
+    
+    protected void RunPanbyte(string[] args)
+    {
+        IConsoleApp app = new PanbyteConsoleApp(args);
+        app.Start();
     }
 }
