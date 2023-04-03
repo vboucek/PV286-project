566aee05ccf38763294574f62b2e47732fd0c47e
Vladimír Bouček <72857024+vladimirboucek@users.noreply.github.com>
2023-04-03 18:59:25 +0200

    refactor: small improvements in input processor

--- Panbyte/Panbyte/InputProcessing/InputProcessor.cs
+++ Panbyte/Panbyte/InputProcessing/InputProcessor.cs
@@ -1,21 +1,21 @@
 using System.Text;
 using Panbyte.Converters;
-using Panbyte.OptionsParsing;
+using Panbyte.Formats;
 
 namespace Panbyte.InputProcessing;
 
 public class InputProcessor
 {
     private readonly IConverter _converter;
-    private readonly PanbyteOptions _options;
+    private readonly Format _outputFormat;
 
-    public InputProcessor(IConverter converter, PanbyteOptions options)
+    public InputProcessor(IConverter converter, Format outputFormat)
     {
         _converter = converter;
-        _options = options;
+        _outputFormat = outputFormat;
     }
-    
-    private int[] KmpTable(string delimiter)
+
+    private static int[] KmpTable(string delimiter)
     {
         var table = new int[delimiter.Length];
         table[0] = -1;
@@ -43,32 +43,32 @@ public class InputProcessor
         return table;
     }
 
-    private void ProcessWithKmp(TextReader reader, TextWriter writer)
+    private void ProcessWithKmp(string delimiter, TextReader reader, TextWriter writer)
     {
         StringBuilder buffer = new(); // Current buffer read from stream
 
         var bufferIndex = 0; // Position in buffer
         var delimiterIndex = 0; // Position in delimiter
-        var table = KmpTable(_options.Delimiter);
+        var table = KmpTable(delimiter);
 
         while (reader.Peek() > 0)
         {
             // Append the next character
             buffer.Append((char)reader.Read());
 
-            if (_options.Delimiter[delimiterIndex] == buffer[bufferIndex])
+            if (delimiter[delimiterIndex] == buffer[bufferIndex])
             {
                 bufferIndex++;
                 delimiterIndex++;
 
-                if (delimiterIndex == _options.Delimiter.Length)
+                if (delimiterIndex == delimiter.Length)
                 {
                     // found delimiter
                     var position = bufferIndex - delimiterIndex;
 
                     writer.Write(_converter.ConvertTo(buffer.Remove(position, buffer.Length - position).ToString(),
-                        _options.OutputFormat));
-                    writer.Write(_options.Delimiter);
+                        _outputFormat));
+                    writer.Write(delimiter);
 
                     // reset buffer
                     bufferIndex = 0;
@@ -89,38 +89,32 @@ public class InputProcessor
         }
 
         // Write the last item (content after last occurrence of delimiter)
-        writer.WriteLine(_converter.ConvertTo(buffer.ToString(), _options.OutputFormat));
+        writer.WriteLine(_converter.ConvertTo(buffer.ToString(), _outputFormat));
     }
 
     private void ProcessEmptyDelimiter(TextReader reader, TextWriter writer)
     {
         while (reader.Peek() > 0)
         {
-            writer.Write(_converter.ConvertTo(((char)reader.Read()).ToString(), _options.OutputFormat));
+            writer.Write(_converter.ConvertTo(((char)reader.Read()).ToString(), _outputFormat));
         }
-        
+
         writer.WriteLine();
     }
 
     private void ProcessWithoutDelimiter(TextReader reader, TextWriter writer)
     {
-        writer.WriteLine(_converter.ConvertTo(reader.ReadToEnd(), _options.OutputFormat));
+        writer.WriteLine(_converter.ConvertTo(reader.ReadToEnd(), _outputFormat));
     }
 
 
-    public void ProcessInput()
+    public void ProcessInput(string? delimiter = null, string? inputFilePath = null, string? outputFilePath = null)
     {
-        if (_options.InputFormat is null || _options.OutputFormat is null)
-        {
-            throw new NullReferenceException("Input and output format cannot be null.");
-        }
-
         // Setup input and output streams (file or stdin/stdout)
-        using var textReader = _options.InputFilePath is null ? Console.In : new StreamReader(_options.InputFilePath);
-        using var textWriter =
-            _options.OutputFilePath is null ? Console.Out : new StreamWriter(_options.OutputFilePath);
+        using var textReader = inputFilePath is null ? Console.In : new StreamReader(inputFilePath);
+        using var textWriter = outputFilePath is null ? Console.Out : new StreamWriter(outputFilePath);
 
-        switch (_options.Delimiter)
+        switch (delimiter)
         {
             case null:
                 // Delimiter is null -> don't take delimiter into account, process whole file
@@ -134,7 +128,7 @@ public class InputProcessor
 
             default:
                 // Delimiter is at least one char long -> use Knuth–Morris–Pratt algorithm
-                ProcessWithKmp(textReader, textWriter);
+                ProcessWithKmp(delimiter, textReader, textWriter);
                 break;
         }
     }
